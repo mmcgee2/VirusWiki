600ad32f69ca0ccf424c5509a06e21677fe1699a
Michael <mcgeemike8@hotmail.com>
2022-07-04 20:29:02 -0700

    Update db.py

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql://ktym57cel7olx1jj:bv8tkko2kdmnpiw6@ebh2y8tqym512wqs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/naahstp8zt2ps63g"
+DATABASE_URL = "mysql://n5az6cwzmic0a8gu:mmzlol2qy6k0dc1v@n4m3x5ti89xl6czh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/dtc39cq7y0wbis8o"
 
 db_engine = create_engine(DATABASE_URL)
 local_session = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)

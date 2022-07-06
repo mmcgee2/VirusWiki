50e37c8386ae44d85f1daa256abef1df3e58d4af
Michael <mcgeemike8@hotmail.com>
2022-07-06 11:41:54 -0700

    Update db.py

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql://mmcgee:QAZwsx!23$@localhost/virus"
+DATABASE_URL = "mysql://pn2qeknluf2bh2xb:lkaqjwxjaazho23z@n4m3x5ti89xl6czh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ll43lin6tfexl1ha"
 
 db_engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 local_session = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)

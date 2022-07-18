6adeacddc953587ec632bd7417ae83d3add6e8ac
Michael <mcgeemike8@hotmail.com>
2022-07-18 02:21:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -36,13 +36,13 @@ def get_db():
 
 @app.post("/virus/", response_model=Schemas.create_variant)
 def create_variants(virus: Schemas.create_variant, db: Session = Depends(get_db)):
-    results = {"virus": virus}
     db_virus = (
         CRUD.get_variant(db, variant=virus.variant),
         CRUD.get_changes(db, changes=virus.changes),
         CRUD.get_description(db, description=virus.description),
         CRUD.get_description2(db, description2=virus.description2),
     )
+    results = {"virus": db_virus}
     if not db_virus:
         raise HTTPException(status_code=400, detail="Variant already in use")
     return results, CRUD.create_variant(db=db, virus=virus)
